4750aa5bc60c04aeca36c8514c1b64eb302472e4
LVRallabandi <67758153+LVRallabandi@users.noreply.github.com>
2020-07-09 20:51:59 +0200

    Update code.ino

--- Arduino/code.ino
+++ Arduino/code.ino
@@ -1,19 +1,16 @@
 void setup() {
   pinMode(13, OUTPUT);    // sets the digital pin 13 as output
-  pinMode(12, OUTPUT);
-  digitalWrite(12, HIGH);
   Serial.begin(9600);
 }
 
 void loop() {
   int sensorValue = analogRead(A0);
-//  if(sensorValue > 0)
-//  {
-//    digitalWrite(13, HIGH);
-//    }
-//   else {
-//    digitalWrite(13, LOW);
-//   }
-  Serial.println(sensorValue);
+  if(sensorValue > 0)
+  {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
   delay(100);        // delay in between reads for stability
 }
